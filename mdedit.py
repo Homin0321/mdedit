13f73f5851d0e8c148382db46b125bb6e9336769
Homin0321 <167429242+Homin0321@users.noreply.github.com>
2024-08-28 13:03:11 +0900

    Update mdedit.py

--- mdedit.py
+++ mdedit.py
@@ -2,13 +2,12 @@ import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Markdown Editor", page_icon="📝")
 
-if "text" not in st.session_state:
-    st.session_state.text = ""
+st.session_state.text = st.session_state.get("text", "")
 
 tab1, tab2 = st.tabs(["Edit", "Preview"])
 
 with tab1:
-    st.session_state.text = st.text_area("Edit Markdown:", st.session_state.text, height=800)
+    st.text_area("Edit Markdown:", key="text", height=800)
 
 with tab2:
-    st.markdown(st.session_state.text, unsafe_allow_html=True)
+    st.markdown(st.session_state.text)
